24b3d9a3ad5a942468ba2c7be92626ae3095e93b
Aziz <azizaliev2337@gmail.com>
2025-01-28 12:32:41 +0500

    refact: added placeholder for inputs phone number

--- src/apps/CustomerSupport/admin.py
+++ src/apps/CustomerSupport/admin.py
@@ -1,89 +1,90 @@
 from django.contrib import admin
-
 from src.config import settings
+from .forms import CustomerRequestAdminForm
 from .models import CustomerRequest
 
 
 @admin.register(CustomerRequest)
 class CustomerRequestAdmin(admin.ModelAdmin):
-    list_display = (
-        "first_name",
-        "last_name",
-        "subject",
-        "request_type",
-        "status",
-        "assigned_to",
-        "incident_date",
-    )
-    list_filter = ("request_type", "status", "service_type", "preferred_contact_method")
-    search_fields = (
-        "first_name",
-        "last_name",
-        "email",
-        "phone_number",
-        "subject",
-        "description",
-        "contract_number",
-    )
-    autocomplete_fields = ("assigned_to",)
+	form = CustomerRequestAdminForm
+	list_display = (
+		"first_name",
+		"last_name",
+		"subject",
+		"request_type",
+		"status",
+		"assigned_to",
+		"incident_date",
+	)
+	list_filter = ("request_type", "status", "service_type", "preferred_contact_method")
+	search_fields = (
+		"first_name",
+		"last_name",
+		"email",
+		"phone_number",
+		"subject",
+		"description",
+		"contract_number",
+	)
+	autocomplete_fields = ("assigned_to",)
 
-    fieldsets = (
-        (
-            None,
-            {"fields": ("first_name", "last_name", "phone_number", "email", "address")},
-        ),
-        (
-            "Детали обращения",
-            {
-                "fields": (
-                    "request_type",
-                    "subject",
-                    "description",
-                    "contract_number",
-                    "service_phone_number",
-                    "service_type",
-                    "attachment",
-                    "preferred_contact_method",
-                    "incident_date",
-                )
-            },
-        ),
-        (
-            "Статус и назначение",
-            {"fields": ("status", "assigned_to", "internal_comment")},
-        ),
-        ("Временные метки", {"fields": ("created_at", "updated_at")}),
-        ("Создано/изменено", {"fields": ("created_by", "updated_by")}),
-    )
+	fieldsets = (
+		(
+			None,
+			{"fields": ("first_name", "last_name", "phone_number", "email", "address")},
+		),
+		(
+			"Детали обращения",
+			{
+				"fields": (
+					"request_type",
+					"subject",
+					"description",
+					"contract_number",
+					"service_phone_number",
+					"service_type",
+					"attachment",
+					"preferred_contact_method",
+					"incident_date",
+				)
+			},
+		),
+		(
+			"Статус и назначение",
+			{"fields": ("status", "assigned_to", "internal_comment")},
+		),
+		("Временные метки", {"fields": ("created_at", "updated_at")}),
+		("Создано/изменено", {"fields": ("created_by", "updated_by")}),
+	)
 
-    def get_readonly_fields(self, request, obj=...):
-        readonly_fields = ["created_at", "updated_at", "created_by", "updated_by"]
+	def get_readonly_fields(self, request, obj=...):
+		readonly_fields = ["created_at", "updated_at", "created_by", "updated_by"]
 
-        if request.user.groups.filter(name=settings.OPERATOR_GROUP_NAME).exists():
-            readonly_fields += ["status", "internal_comment"]
+		if request.user.groups.filter(name=settings.OPERATOR_GROUP_NAME).exists():
+			readonly_fields += ["status", "internal_comment"]
 
-        elif request.user.groups.filter(name=settings.BACK_OFFICE_GROUP_NAME).exists():
-            readonly_fields += [
-                "first_name",
-                "last_name",
-                "phone_number",
-                "email",
-                "address",
-                "request_type",
-                "subject",
-                "description",
-                "contract_number",
-                "service_phone_number",
-                "service_type",
-                "attachment",
-                "preferred_contact_method",
-                "incident_date",
-            ]
+		elif request.user.groups.filter(name=settings.BACK_OFFICE_GROUP_NAME).exists():
+			readonly_fields += [
+				"first_name",
+				"last_name",
+				"phone_number",
+				"email",
+				"address",
+				"request_type",
+				"subject",
+				"description",
+				"contract_number",
+				"service_phone_number",
+				"service_type",
+				"attachment",
+				"preferred_contact_method",
+				"incident_date",
+			]
 
-        return readonly_fields
+		return readonly_fields
 
-    def save_model(self, request, obj, form, change):
-        if not obj.pk:
-            obj.created_by = request.user
-        obj.updated_by = request.user
-        super().save_model(request, obj, form, change)
+	def save_model(self, request, obj, form, change):
+		if not obj.pk:
+			obj.created_by = request.user
+		obj.updated_by = request.user
+		super().save_model(request, obj, form, change)
--- src/apps/CustomerSupport/forms.py
+++ src/apps/CustomerSupport/forms.py
@@ -3,78 +3,88 @@ from .models import CustomerRequest
 from django.utils.translation import gettext_lazy as _
 
 
+class CustomerRequestAdminForm(forms.ModelForm):
+	class Meta:
+		model = CustomerRequest
+		fields = '__all__'
+		widgets = {
+			'phone_number': forms.TextInput(attrs={'placeholder': '+992XXXXXXXXX'}),
+			'service_phone_number': forms.TextInput(attrs={'placeholder': '+992XXXXXXXXX'}),
+		}
+
+
 class CustomerEditRequestForm(forms.ModelForm):
-    class Meta:
-        model = CustomerRequest
-        fields = [
-            "first_name",
-            "last_name",
-            "phone_number",
-            "email",
-            "address",
-            "request_type",
-            "subject",
-            "description",
-            "contract_number",
-            "service_phone_number",
-            "service_type",
-            "attachment",
-            "preferred_contact_method",
-            "incident_date",
-            "status",
-            "assigned_to",
-            "internal_comment",
-        ]
-        widgets = {
-            "description": forms.Textarea(attrs={"rows": 4}),
-            "internal_comment": forms.Textarea(attrs={"rows": 4}),
-            "incident_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-        }
+	class Meta:
+		model = CustomerRequest
+		fields = [
+			"first_name",
+			"last_name",
+			"phone_number",
+			"email",
+			"address",
+			"request_type",
+			"subject",
+			"description",
+			"contract_number",
+			"service_phone_number",
+			"service_type",
+			"attachment",
+			"preferred_contact_method",
+			"incident_date",
+			"status",
+			"assigned_to",
+			"internal_comment",
+		]
+		widgets = {
+			"description": forms.Textarea(attrs={"rows": 4}),
+			"internal_comment": forms.Textarea(attrs={"rows": 4}),
+			"incident_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+		}
 
 
 class CustomerRequestForm(forms.ModelForm):
-    class Meta:
-        model = CustomerRequest
-        fields = [
-            "first_name",
-            "last_name",
-            "phone_number",
-            "email",
-            "address",
-            "request_type",
-            "subject",
-            "description",
-            "contract_number",
-            "service_phone_number",
-            "service_type",
-            "attachment",
-            "preferred_contact_method",
-            "incident_date",
-            "status",
-            "assigned_to",
-            "internal_comment",
-        ]
-        widgets = {
-            "incident_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
-            "description": forms.Textarea(attrs={"rows": 3}),
-            "internal_comment": forms.Textarea(attrs={"rows": 3}),
-        }
-        labels = {
-            "first_name": _("Имя"),
-            "last_name": _("Фамилия"),
-            "phone_number": _("Номер телефона"),
-            "email": _("Электронная почта"),
-            "address": _("Адрес"),
-            "request_type": _("Тип обращения"),
-            "subject": _("Тема"),
-            "description": _("Описание проблемы"),
-            "contract_number": _("Лицевой счет/договор"),
-            "service_phone_number": _("Номер телефона услуги"),
-            "service_type": _("Тип услуги"),
-            "attachment": _("Прикрепленный файл"),
-            "preferred_contact_method": _("Предпочтительный способ связи"),
-            "incident_date": _("Дата и время инцидента"),
-            "status": _("Статус"),
-            "assigned_to": _("Ответственное лицо"),
-            "internal_comment": _("Комментарий сотрудника"),
-        }
+	class Meta:
+		model = CustomerRequest
+		fields = [
+			"first_name",
+			"last_name",
+			"phone_number",
+			"email",
+			"address",
+			"request_type",
+			"subject",
+			"description",
+			"contract_number",
+			"service_phone_number",
+			"service_type",
+			"attachment",
+			"preferred_contact_method",
+			"incident_date",
+			"status",
+			"assigned_to",
+			"internal_comment",
+		]
+		widgets = {
+			"incident_date": forms.DateTimeInput(attrs={"type": "datetime-local"}),
+			"description": forms.Textarea(attrs={"rows": 3}),
+			"internal_comment": forms.Textarea(attrs={"rows": 3}),
+		}
+		labels = {
+			"first_name": _("Имя"),
+			"last_name": _("Фамилия"),
+			"phone_number": _("Номер телефона"),
+			"email": _("Электронная почта"),
+			"address": _("Адрес"),
+			"request_type": _("Тип обращения"),
+			"subject": _("Тема"),
+			"description": _("Описание проблемы"),
+			"contract_number": _("Лицевой счет/договор"),
+			"service_phone_number": _("Номер телефона услуги"),
+			"service_type": _("Тип услуги"),
+			"attachment": _("Прикрепленный файл"),
+			"preferred_contact_method": _("Предпочтительный способ связи"),
+			"incident_date": _("Дата и время инцидента"),
+			"status": _("Статус"),
+			"assigned_to": _("Ответственное лицо"),
+			"internal_comment": _("Комментарий сотрудника"),
+		}
