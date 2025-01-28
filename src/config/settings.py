654b34e913bf1d050b587f57f21d7199d2284e61
Aziz <azizaliev2337@gmail.com>
2025-01-28 14:52:00 +0500

    refactoring

--- src/config/settings.py
+++ src/config/settings.py
@@ -10,8 +10,8 @@ SECRET_KEY = os.getenv("DJANGO_SECRET_KEY")
 
 DEBUG = bool(os.getenv("DJANGO_ENV") == "development")
 
-# ALLOWED_HOSTS = ['localhost', '0.0.0.0']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '0.0.0.0']
+# ALLOWED_HOSTS = []
 
 LOGIN_REDIRECT_URL = "customer_support:requests_list"
 LOGOUT_REDIRECT_URL = "users:login"
@@ -66,22 +66,22 @@ WSGI_APPLICATION = "src.config.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
 # DATABASES = {
-# 	'default': {
-# 		'ENGINE': 'django.db.backends.postgresql',
-# 		'NAME': os.getenv("POSTGRES_DB"),
-# 		'USER': os.getenv("POSTGRES_USER"),
-# 		'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
-# 		'HOST': os.getenv("POSTGRES_HOST"),
-# 		'PORT': os.getenv("POSTGRES_PORT"),
-# 	}
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
 # }
+DATABASES = {
+	'default': {
+		'ENGINE': 'django.db.backends.postgresql',
+		'NAME': os.getenv("POSTGRES_DB"),
+		'USER': os.getenv("POSTGRES_USER"),
+		'PASSWORD': os.getenv("POSTGRES_PASSWORD"),
+		'HOST': os.getenv("POSTGRES_HOST"),
+		'PORT': os.getenv("POSTGRES_PORT"),
+	}
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
